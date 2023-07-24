c8476077680f41886293db131575be332c825655
Pruthviraj Chaudhari <chaudharipruthviraj888@gmail.com>
2023-07-24 21:52:01 +0530

    Level 3. Authentification using Hashing (md5)

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -25,9 +25,6 @@ const userSchema = new mongoose.Schema({
     password: { type: String, required: true }
   });
 
-const secretKey = process.env.SECRET_KEY;
-
-userSchema.plugin(encrypt, { secret: secretKey, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -53,7 +50,7 @@ app.post("/register", (req, res)=>{
     
     const newUser = new User({
         email : req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save()
@@ -69,7 +66,7 @@ app.post("/register", (req, res)=>{
 
 app.post("/login", async (req, res) => {
     const email = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
   
     User.findOne({ email })
     .then((user) => {
