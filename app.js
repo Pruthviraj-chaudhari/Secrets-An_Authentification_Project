f7a23ae9180c92c79c43db7eec1f6aa2eb3a2faf
Pruthviraj Chaudhari <chaudharipruthviraj888@gmail.com>
2023-07-25 14:56:18 +0530

    Level 5. Authentification with Passport & Session

--- app.js
+++ app.js
@@ -3,102 +3,106 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require('passport-local').Strategy;
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+  secret: "My secret",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(()=>{
-        console.log("Successfuly connected to database.");
-    })
-    .catch(err=>{
-        console.log("Error connecting to database:", err);
-    });
-
-
-const userSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+// MongoDB connection
+mongoose.connect("mongodb://127.0.0.1:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Successfully connected to the database.");
+  })
+  .catch(err => {
+    console.log("Error connecting to the database:", err);
   });
 
+  
+// User model and passport setup
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true
+  },
+  password: {
+    type: String
+  }
+});
 
+userSchema.plugin(passportLocalMongoose);
 const User = new mongoose.model("User", userSchema);
 
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-app.get("/", (req, res)=>{
-    res.render("home");
+
+// Routes
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
-app.get("/login", (req, res)=>{
-    res.render("login");
+app.get("/login", (req, res) => {
+  res.render("login");
 });
 
-app.get("/register", (req, res)=>{
-    res.render("register");
+app.get("/register", (req, res) => {
+  res.render("register");
 });
 
-app.get("/logout", (req, res)=>{
-    res.render("home");
+app.get("/logout", (req, res) => {
+    req.logout(() => {
+        res.redirect("/");
+    });    
 });
 
-const saltRounds = 10;
+app.get("/secrets", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
 
-app.post("/register", (req, res)=>{
-    
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-
-      const newUser = new User({
-        email : req.body.username,
-        password: hash   //save generated hash as password
-      });
-
-      newUser.save()
-          .then(()=>{
-              console.log("Account created successfuly");
-              res.render("secrets");
-          })
-          .catch(err=>{
-              console.log("Error creating account: ", err);
-          });
+// Post 
+app.post('/register', (req, res) => {
+  User.register(new User({ username: req.body.username }), req.body.password, (err, user) => {
+    if (err) {
+      console.error(err);
+      return res.redirect('/register');
+    }
+    passport.authenticate('local')(req, res, () => {
+      res.redirect('/secrets');
     });
-});
-
-
-app.post("/login", async (req, res) => {
-    const email = req.body.username;
-    const password = req.body.password;
-  
-    User.findOne({ email })
-    .then((user) => {
-
-      if (!user) {
-        return res.status(404).json({ error: "User not found" });
-      }
-
-      bcrypt.compare(password, user.password, (err, result)=>{
-        if(result)
-            return res.status(200).render("secrets");
-        else
-          return res.status(401).json({ error: "Invalid password" });
-        
-      });
-    })
-    .catch((err) => {
-
-      console.error("Error during login:", err);
-      res.status(500).json({ error: "Internal server error" });
-      
-    });
-
   });
+});
 
+app.post('/login', passport.authenticate('local', {
+      successRedirect: '/secrets', 
+      failureRedirect: '/login'
+    })
+);
 
-  
-app.listen(3000, ()=>{
-    console.log("Server listening on port 3000");
-})
+// 404 error handler
+app.use((req, res) => {
+  res.status(404).send("Page not found");
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
