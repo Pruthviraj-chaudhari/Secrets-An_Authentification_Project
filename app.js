f399e7743c5759c166a54868d0f3cf513d21725c
Pruthviraj Chaudhari <chaudharipruthviraj888@gmail.com>
2023-07-26 00:08:29 +0530

    Level 6. Authentification with Google & GitHub

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require('passport-local').Strategy;
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const GitHubStrategy = require('passport-github').Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -22,7 +25,6 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 // MongoDB connection
 mongoose.connect("mongodb://127.0.0.1:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
@@ -32,30 +34,98 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB", { useNewUrlParser: true, us
     console.log("Error connecting to the database:", err);
   });
 
-  
 // User model and passport setup
 const userSchema = new mongoose.Schema({
   username: {
-    type: String,
-    unique: true
+    type: String
   },
   password: {
     type: String
-  }
+  },
+  googleId: String,
+  githubId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
-const User = new mongoose.model("User", userSchema);
+userSchema.plugin(findOrCreate);
+
+const User = mongoose.model("User", userSchema);
 
 passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
 
+passport.use(new GoogleStrategy({
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/google/secrets",
+  userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+},
+  (accessToken, refreshToken, profile, done) => {
+    const { id, displayName } = profile;
+    User.findOrCreate({ googleId: id, username: displayName }, function (err, user) {
+      if (err) {
+        return done(err);
+      }
+      return done(null, user);
+    });
+  }
+));
 
-// Routes
+passport.use(new GitHubStrategy({
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: "/auth/github/secrets"
+},
+  (accessToken, refreshToken, profile, done) => {
+    const { id, displayName } = profile;
+    User.findOrCreate({ githubId: id, username: displayName }, function (err, user) {
+      if (err) {
+        return done(err);
+      }
+      return done(null, user);
+    });
+  }
+));
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id)
+    .then(user => done(null, user))
+    .catch(err => done(err, null));
+});
+
+// Routes for Google login
 app.get("/", (req, res) => {
   res.render("home");
 });
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+app.get('/auth/google/secrets',
+  passport.authenticate('google', { failureRedirect: "/login" }),
+  (req, res) => {
+    // Successful authentication
+    res.redirect("/secrets");
+  }
+);
+
+
+// Routes for GitHub login
+app.get('/auth/github',
+  passport.authenticate('github')
+);
+
+app.get('/auth/github/secrets',
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  (req, res) => {
+    // Successful authentication
+    res.redirect('/secrets');
+  }
+);
 
 app.get("/login", (req, res) => {
   res.render("login");
@@ -66,9 +136,9 @@ app.get("/register", (req, res) => {
 });
 
 app.get("/logout", (req, res) => {
-    req.logout(() => {
-        res.redirect("/");
-    });    
+  req.logout(() => {
+    res.redirect("/");
+  });
 });
 
 app.get("/secrets", (req, res) => {
@@ -79,9 +149,11 @@ app.get("/secrets", (req, res) => {
   }
 });
 
-// Post 
+// Post
 app.post('/register', (req, res) => {
-  User.register(new User({ username: req.body.username }), req.body.password, (err, user) => {
+  // Use the provided username from the registration form
+  const { username, password } = req.body;
+  User.register(new User({ username }), password, (err, user) => {
     if (err) {
       console.error(err);
       return res.redirect('/register');
@@ -93,10 +165,9 @@ app.post('/register', (req, res) => {
 });
 
 app.post('/login', passport.authenticate('local', {
-      successRedirect: '/secrets', 
-      failureRedirect: '/login'
-    })
-);
+  successRedirect: '/secrets',
+  failureRedirect: '/login'
+}));
 
 // 404 error handler
 app.use((req, res) => {
